5de8318ffad88c7646671a005ef7c621a05cf656
SantiNones <santiagonones@gmail.com>
2025-12-03 22:11:29 +0000

    StarWars Data Model Final

--- src/models.py
+++ src/models.py
@@ -1,19 +1,52 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+import os 
+import sys 
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, declarative_base 
+from eralchemy2 import render_er
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(120), nullable=False)
+    is_active = Column(Boolean(), nullable=False)
+
+class Planet(Base):
+    __tablename__ = "planet"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    climate = Column(String(120))
+    terrain = Column(String(120))
+    population = Column(String(120))
+
+    residents = relationship("Character", back_populates="homeworld")
+
+class Character(Base):
+    __tablename__ = "character"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    homeworld_id = Column(Integer, ForeignKey("planet.id"), nullable=False)
+
+    homeworld = relationship("Planet", back_populates="residents")
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    character_id = Column(Integer, ForeignKey("character.id"))
+
+    user = relationship("User")
+    planet = relationship("Planet")
+    character = relationship("Character")
+
+render_er(Base, 'diagram.png')
+
